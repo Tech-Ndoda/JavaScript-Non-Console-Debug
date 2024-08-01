f5e02435f591692021e241a053e02c6c9a62d544
techNdoda <kuhlematolengwe@gmail.com>
2024-08-01 23:08:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/*
+   This function is supposed to add two variables: "a" and "b", then return their sum.
+   However, the developer makes a mistake and uses variable "d" instead of "b", and
+   "d" isn't declared which leads to an error thrown.
+
+   So, this error won't he shown to the developer at run stage if it is not logged, or say,
+   the IDE doesn't have a console.
+*/
+
 function add(a, b)
 {
   return(a + d);
