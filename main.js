814135311effc04679f9deb4d24b3e0b2ee17292
techNdoda <kuhlematolengwe@gmail.com>
2024-08-01 23:14:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
    However, the developer makes a mistake and uses variable "d" instead of "b", and
    "d" isn't declared which leads to an error thrown.
 
-   So, this error won't he shown to the developer at run stage if it is not logged, or say,
+   So, this error won't be shown to the developer at run stage if it is not logged, or say,
    the IDE doesn't have a console.
 */
 
